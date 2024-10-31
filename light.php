89033f16764076dded4670c3b327cc3182a000dd
tanakahauka <s2232098jm@s.chibakoudai.jp>
2024-10-31 16:10:48 +0900

    ライトの付き具合によって画像が変化する処理の作成

--- light.php
+++ light.php
@@ -3,9 +3,8 @@
         $greenlamp = $_GET["green"];
         $redlamp = $_GET["red"];
         $bluelamp = $_GET["blue"];
-        $btn = $_GET["btn"];
         //ボタンによるライトの変更
-        switch($btn){
+        switch($_GET["btn"]){
             case "1":
                 $greenlamp = !$greenlamp;
                 $bluelamp = !$bluelamp;
@@ -34,12 +33,32 @@
 <body>
     <div>
         <!--画像表示-->
+        <?php
+            if($redlamp && $greenlamp && $bluelamp){
+                $img = "img/white_t.jpg";
+            }else if($redlamp){
+                if($greenlamp) $img = "img/yellow_t.jpg";
+                else if($bluelamp) $img = "img/magenta_t.jpg";
+                else $img = "img/redlight_tst.jpg";
+            }else if($greenlamp){
+                if($bluelamp) $img = "img/cyan_t.jpg";
+                else $img = "img/greenlight_tst.jpg";
+            }else if($bluelamp){
+                $img = "img/bluelight_t.jpg";
+            }else {
+                $img = "img/black_t.jpg";
+            }
+            echo " <img src=\"" .$img. "\"alt=\"\"> ";
+        ?>
+        <p>緑：<?php echo $greenlamp ?></p>
+        <p>赤：<?php echo $redlamp ?></p>
+        <p>青：<?php echo $bluelamp ?></p>
     </div>
     <div class="buttons">
         <form method="get" action="light.php">
-            <button type="submit" name="btn">1</button>
-            <button type="submit" name="btn">2</button>
-            <button type="submit" name="btn">3</button>
+            <button type="submit" value="1" name="btn">1</button>
+            <button type="submit" value="2" name="btn">2</button>
+            <button type="submit" value="3" name="btn">3</button>
             <input type="hidden" value="<?php echo $greenlamp ?>" name="green">
             <input type="hidden" value="<?php echo $redlamp ?>" name="red">
             <input type="hidden" value="<?php echo $bluelamp ?>" name="blue">
